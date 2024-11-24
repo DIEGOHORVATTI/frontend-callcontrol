4e8993f6f929c4c8875410f1bf7b9995fb70def0
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-11-24 14:59:14 -0300

    refactor: simplify Home component by removing settings context and updating content

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,17 +1,7 @@
-import { Button } from '@mui/material'
-
-import { useSettingsContext } from '@/contexts/settings'
-
 export const Home = () => {
-  const settings = useSettingsContext()
-
   return (
     <div>
-      <h1>{`${settings.themeMode} mode!`}</h1>
-
-      <Button variant="soft" onClick={settings.onToggleMode}>
-        mode
-      </Button>
+      <h1>aqui fica o comteudo</h1>
     </div>
   )
 }
