6a92bb8f5fef400d5ec4cb6a1b0ded11e832456f
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-11-26 03:48:06 -0300

    refactor: enhance AuthGuard to use useEffect for navigation based on authentication state

--- src/guards/AuthGuard.tsx
+++ src/guards/AuthGuard.tsx
@@ -1,19 +1,21 @@
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { useAuth } from '../contexts/auth-provider'
+import { useAuth } from '@/contexts/auth-provider'
 
 export const AuthGuard = ({ children }: React.PropsWithChildren) => {
+  const { isAuthenticated } = useAuth()
   const navigate = useNavigate()
 
-  const { isAuthenticated } = useAuth()
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/auth')
 
-  if (!isAuthenticated) {
-    navigate('/auth')
-  }
+      return
+    }
 
-  if (isAuthenticated) {
     navigate('/')
-  }
+  }, [isAuthenticated, navigate])
 
-  return <>{children}</>
+  return children
 }
